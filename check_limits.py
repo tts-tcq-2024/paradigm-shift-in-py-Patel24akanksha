15bb5b96683e382e9bc1edea14d82cf1be657dd0
Patel24akanksha <86720877+Patel24akanksha@users.noreply.github.com>
2024-08-12 12:33:23 +0530

    Update check_limits.py

--- check_limits.py
+++ check_limits.py
@@ -11,5 +11,5 @@ def battery_is_ok(temperature, soc, charge_rate):
     return is_temperature_ok(temperature) and is_soc_ok(soc) and is_charge_rate_ok(charge_rate)
 
 if __name__ == '__main__':
-    assert battery_is_ok(25, 70, 0.7) is True
+    assert battery_is_ok(25, 80, 0.7) is True
     assert battery_is_ok(50, 85, 0) is False
